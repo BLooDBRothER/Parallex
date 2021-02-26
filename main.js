834d91cbfd9073e9338827fbe22a70c358e81185
BLooDBRothER <computertechnology446@gmail.com>
2021-02-26 11:49:39 +0530

    Added onclick functionality for the button to open and close the navbar

--- main.js
+++ main.js
@@ -9,3 +9,19 @@ if (isMobile) {
     ig.style.backgroundSize = "contain";
     ig.style.backgroundRepeat = "norepeat";
 }
+
+function toggleopen(){
+    var hg = document.getElementById("cnt-hg");
+    var mr = document.getElementById("title");
+    var ic = document.getElementById("m-o");
+    if(hg.className == "container"){
+        hg.className += " hg";
+        mr.style.marginBottom = "1rem";
+        ic.style.transform = "rotate(180deg)";
+    }
+    else{
+        hg.className = "container";
+        mr.style.marginBottom = "4rem";
+        ic.style.transform = "rotate(0deg)";
+    }
+}
